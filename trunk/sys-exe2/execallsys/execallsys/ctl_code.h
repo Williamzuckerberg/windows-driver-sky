cb52d62346f2d551ec0dfa2509fc8369084503de
topgiftie@gmail.com <topgiftie@gmail.com@3a3cd65c-1fc1-b21e-901d-6b6205d3bd68>
2011-08-21 14:04:51 +0000

    菜鸟之驱动开发8

---
+++ trunk/sys-exe2/execallsys/execallsys/ctl_code.h
@@ -0,0 +1,14 @@
+/*#ifndef  CTL_CODE
+#pragma message("\n\n------------------exe 模式.please include winioctl.h")
+#include<winioctl.h> //CTL_CODE
+#else
+#pragma message("\n\n------------------sys 模式.no need to include winioctl.h")
+#endif*/
+#define add_code CTL_CODE(FILE_DEVICE_UNKNOWN, 	0x800, 	METHOD_IN_DIRECT,FILE_ANY_ACCESS)
+#define sub_code CTL_CODE(FILE_DEVICE_UNKNOWN, 	0x801, 	METHOD_IN_DIRECT,FILE_ANY_ACCESS)
+
+//有四种方式访问模式
+//#define METHOD_BUFFERED                 0  缓冲区方式
+//#define METHOD_IN_DIRECT                1  直接访问方式
+//#define METHOD_OUT_DIRECT               2  直接访问方式
+//#define METHOD_NEITHER                  3  其它访问方式
---
+++ trunk/sys-exe2/execallsys/execallsys/execallsys.cpp
@@ -0,0 +1,46 @@
+// execallsys.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+#include "ctl_code.h"
+
+int add(HANDLE hDevice, int a,int b)
+{
+
+	int port[2];
+	int bufret;
+	ULONG dwWrite;
+	port[0]=a;
+	port[1]=b;
+
+	DeviceIoControl(hDevice, add_code , &port, 8, &bufret, 4, &dwWrite, NULL);
+	return bufret;
+
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	HANDLE hDevice = 
+		CreateFile(L"\\\\.\\firstSysDevice", //\\??\\firstSysDevice
+		GENERIC_READ | GENERIC_WRITE,
+		0,		// share mode none
+		NULL,	// no security
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL );		// no template
+	printf("start\n");
+	if (hDevice == INVALID_HANDLE_VALUE)
+	{
+		printf("获取驱动句柄失败: %s with Win32 error code: %d\n","MyDriver", GetLastError() );
+		getchar();
+		return -1;
+	}
+	int a=9955;
+	int b=33;
+	int r=add(hDevice,a,b);
+	printf("%d+%d=%d \n",a,b,r);
+	getchar();
+	return 0;
+}
+
